0bbb44ee6e79344a666bc4b65b8f08dd0767ef29
Abdullah abo elhija <hija368@gmail.com>
2025-11-14 15:16:20 +0200

    updating the tour schema and import the data to mongoDB

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,19 +1,61 @@
 const mongoose = require('mongoose');
 
 const tourSchema = new mongoose.Schema({
-    name: {
+  name: {
     type: String,
     required: [true, 'A tour must have a name'],
-    unique: true
-    },
-    rating: {
+    unique: true,
+    trim: true,
+  },
+  duration: {
     type: Number,
-    default: 4.5
-    },
-    price: {
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
     type: Number,
-    required: [true, 'A tour must have a price']
-    }
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+    enum: {
+      values: ['easy', 'medium', 'difficult'],
+      message: 'Difficulty is either: easy, medium, difficult',
+    },
+  },
+  ratingAverage: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingQuantity: {
+    type: Number,
+    default: 0,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a summary'],
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a description'],
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 const Tour = mongoose.model('Tour', tourSchema);
 
--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-
-
 const DB = process.env.DATABASE_HOST.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
@@ -13,9 +11,7 @@ const DB = process.env.DATABASE_HOST.replace(
 mongoose
   .connect(DB)
   .then(() => console.log('DB connection successful!'))
-  .catch(err => console.error('DB connection error:', err));
-
-
+  .catch((err) => console.error('DB connection error:', err));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
